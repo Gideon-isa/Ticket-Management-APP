b2b531d3a09ef0a92176167da9bd30dc1a744117
Gideon Isa <innocentgideon10@gmail.com>
2024-01-07 16:29:49 +0100

    added the domain entities

---
+++ GlobalTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaces { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
